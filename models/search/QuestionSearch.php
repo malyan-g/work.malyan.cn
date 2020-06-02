91285c1c54714a0f3bd0652ebaab91c204448e8e
maliang <maliang@xywy.com>
2020-06-02 15:44:09 +0800

    ml

--- models/search/QuestionSearch.php
+++ models/search/QuestionSearch.php
@@ -135,7 +135,8 @@ class QuestionSearch extends Question
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
             'pagination' => [
-                'pageSize' => ArrayHelper::getValue($params, 'per-page', 10)
+                'pageSize' => ArrayHelper::getValue($params, 'per-page', 10),
+                'page' => ArrayHelper::getValue($params, 'page', 1)
             ],
         ]);
         $this->number = $dataProvider->pagination->limit * $dataProvider->pagination->offset;
