f01d3faf8a03bca98e7fe238804874c1a2ae63de
maliang <maliang@xywy.com>
2020-06-02 15:46:29 +0800

    ml

--- models/search/QuestionSearch.php
+++ models/search/QuestionSearch.php
@@ -139,7 +139,7 @@ class QuestionSearch extends Question
                 'page' => ArrayHelper::getValue($params, 'page', 1)
             ],
         ]);
-        $this->number = $dataProvider->pagination->limit * $dataProvider->pagination->offset;
+        $this->number = $dataProvider->pagination->offset;
 
         $query->where([self::tableName() . '.status' => 1, QuestionCate::tableName() . '.status' => 1]);
         $query->orderBy([self::tableName() . '.created_at' => SORT_DESC]);
