a90e089661687d5079c0c7a0490303c537a649c1
maliang <maliang@xywy.com>
2020-06-02 15:51:44 +0800

    ml

--- controllers/QuestionController.php
+++ controllers/QuestionController.php
@@ -56,7 +56,7 @@ class QuestionController extends Controller
     {
         $searchModel = new QuestionSearch();
         $dataProvider = $searchModel->cateSearch(Yii::$app->request->queryParams);
-        var_dump($dataProvider->pagination->limit);
+        var_dump($dataProvider->pagination->offset);
         $this->number = $dataProvider->pagination->limit + 1;
         return $this->render('cate-index', [
             'searchModel' => $searchModel,
