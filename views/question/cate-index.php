8b0ac057d780b70e335891c36121d5fd8a8ba40f
maliang <maliang@xywy.com>
2020-06-02 15:31:46 +0800

    ml

--- views/question/cate-index.php
+++ views/question/cate-index.php
@@ -20,7 +20,8 @@ JqueryIsa::widget();
     'dataProvider' => $dataProvider,
     'layout' => '<ul class="ranking-list">{items}</ul>{pager}',
     'itemView' => function($model, $key, $index){
-        $number = (int) $this->context->number + $index + 1;
+        $number =  $this->context->number + $index + 1;
+        var_dump($number);
         if($number < 10){
             $number = '0' . $number;
         }
