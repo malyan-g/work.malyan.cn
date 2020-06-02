c93ec6aaa9daa8b8f4837fe0ab7ea9b9d4426036
maliang <maliang@xywy.com>
2020-06-02 12:00:00 +0800

    ml

--- views/question/cate-index.php
+++ views/question/cate-index.php
@@ -21,6 +21,7 @@ $number = 1;
     'dataProvider' => $dataProvider,
     'layout' => '<ul class="ranking-list">{items}</ul>{pager}',
     'itemView' => function($model){
+    var_dump($model);die;
         $html = '<li class="ranking-content">
                             <div class="ranking-title">
                                 <span class="ranking-number">'. $model->number .'</span>
