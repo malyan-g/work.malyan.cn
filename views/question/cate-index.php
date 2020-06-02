723539abb4be6a75de4585bc8d7401cf60287886
maliang <maliang@xywy.com>
2020-06-02 14:33:28 +0800

    ml

--- views/question/cate-index.php
+++ views/question/cate-index.php
@@ -20,8 +20,8 @@ $number = 1;
 <?= $dataProvider->query->count() ? ListView::widget([
     'dataProvider' => $dataProvider,
     'layout' => '<ul class="ranking-list">{items}</ul>{pager}',
-    'itemView' => function($model,$dataProvider){
-    var_dump($dataProvider);die;
+    'itemView' => function($model, $key, $value){
+    var_dump($key,$value);die;
         $html = '<li class="ranking-content">
                             <div class="ranking-title">
                                 <span class="ranking-number">'. $model->number .'</span>
