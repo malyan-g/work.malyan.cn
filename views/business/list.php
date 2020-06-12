c64ddddcb03cd527b97a3e0ad92d3e6d9bb63d97
maliang <maliang@xywy.com>
2020-06-12 10:35:25 +0800

    ml

--- views/business/list.php
+++ views/business/list.php
@@ -3,10 +3,8 @@
 /* @var $dataProvider \yii\data\ActiveDataProvider */
 /* @var $searchModel \app\models\search\QuestionSearch */
 
-use yii\helpers\Url;
 use yii\bootstrap\Html;
 use yii\grid\GridView;
-use yii\bootstrap\Modal;
 
 $this->title = 'Business List';
 $this->params['breadcrumbs'][] = $this->title;
@@ -39,7 +37,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'format' => 'raw',
             'enableSorting'=>false,
             'value' => function($model){
-                $label = Html::a('View', 'javascript:void(0)', ['class'=>'btn btn-info business-modal', 'data-toggle' => 'modal', 'data-target' => '#business-modal', 'data-id' => $model->id]);
+                $label = Html::a('View', ['business/view', 'id'=>$model->id], ['class'=>'btn btn-info']);
                 if($this->context->userId == $model->user_id){
                     $label .= Html::a('Update', ['business/update', 'id'=>$model->id], ['class'=>'btn btn-warning', 'target' => '_blank']);
                     $label .= Html::a('Delete',['business/delete', 'id'=>$model->id], ['class'=>'btn btn-danger', 'data-confirm' => 'Confirm Delete?' ]);
@@ -50,26 +48,3 @@ $this->params['breadcrumbs'][] = $this->title;
         ]
     ]
 ]) ?>
-
-<?php
-Modal::begin([
-    'id' => 'business-modal',
-    'header' => '<h4 class="modal-title">内容</h4>',
-    'closeButton' => false
-]);
-$requestUrl = Url::toRoute(['business/view']);
-$js = <<<JS
-    $(".business-modal").click(function(){
-        var id = $(this).data('id');
-        $.get('{$requestUrl}', {id: id},
-            function (data) {
-                $('.modal-body').html(data);
-            }  
-        );
-    });
-JS;
-$this->registerJs($js);
-Modal::end();
-?>
-
-
