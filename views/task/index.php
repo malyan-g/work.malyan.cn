bdd73d789cd1bacb5f3573f83a499bf5d44c9fa6
maliang <maliang@xywy.com>
2020-06-22 16:53:37 +0800

    ml

--- views/task/index.php
+++ views/task/index.php
@@ -21,6 +21,16 @@ $this->params['breadcrumbs'][] = $this->title;
     </div>
 </div>
 <?php
+$JSLoading = <<<EOF
+    function(bool){
+        if (bool){
+            $(".calendarWrapper").show();
+        }else{
+            $(".calendarWrapper").fadeOut();
+        }
+    }
+EOF;
+
 $JSDayClick = <<<EOF
     function(dayDate, allDay, jsEvent, view) {
         var dayDate = new Date(dayDate._d);
@@ -273,6 +283,7 @@ EOF;
         'displayEventTime' => true,
         'displayEventEnd' => true,
         'timeFormat' => 'HH:mm',
+        'loading' => new JsExpression($JSLoading),
         'dayClick' => new JsExpression($JSDayClick),
     ],
     //'themeSystem' => 'standard',
